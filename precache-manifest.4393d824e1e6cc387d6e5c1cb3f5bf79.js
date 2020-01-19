40cbe3acb30a6147a6d87be91de52957960dfcd2
nelo <nelokwaz@gmail.com>
2020-01-19 16:56:44 +0100

    Updates

---
+++ precache-manifest.4393d824e1e6cc387d6e5c1cb3f5bf79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0829c4b92ac8c57f9e3f2626e6db8077",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8defa6bb5b2734890327",
+    "url": "/react-monsters-rolodex/static/css/main.79792485.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/react-monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8defa6bb5b2734890327",
+    "url": "/react-monsters-rolodex/static/js/main.2297ccb8.chunk.js"
+  },
+  {
+    "revision": "a17f75d38f92581ed444",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.7e4f218e.js"
+  }
+]);
